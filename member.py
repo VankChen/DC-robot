25ea8515b6aed3028e6863de005dfcd3a23a3de1
Vank Chen <sochensun92hm@gmail.com>
2022-03-16 01:41:44 +0800

    Update member.py

--- member.py
+++ member.py
@@ -1,7 +1,10 @@
 @bot.event
 async def on_member_join(member):
-    print(f"{member}歡迎加入邊緣垃圾群")
+    channel=bot.get_channel(953135766974894120)
+    await channel.send(f"{member}歡迎加入邊緣垃圾群")
 
 @bot.event
 async def on_member_remove(member):
+    channel=bot.get_channel(953135766974894120)
+    await channel.send(f"{member}沒差啊我才不稀罕要滾就滾")
     print(f"{member}沒差啊我才不稀罕要滾就滾")
