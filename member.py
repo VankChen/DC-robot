381734bf154862f2791d1359d62443cbbecb0b1e
Vank Chen <sochensun92hm@gmail.com>
2022-03-16 01:54:57 +0800

    Update member.py

--- member.py
+++ member.py
@@ -7,3 +7,8 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel=bot.get_channel(953135766974894120)
     await channel.send(f"{member}沒差啊我才不稀罕要滾就滾")
+
+#因版本更新，需要加寫intents
+#intents=discord.intents.default()
+#intents.members=True
+#bot=commands.Bot(command_prefix="[",intents=intents)
